3421e6b0fac567be630edbeb544e8376d06e25d6
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2013-10-05 02:18:09 -0500

    Adding new functions

---
+++ sample1/process_gif/processGIF.py
@@ -0,0 +1,22 @@
+
+
+from PIL import Image
+im = Image.open('images/wolf.gif')
+
+def iter_frames(im):
+    try:
+        i= 0
+        while 1:
+            im.seek(i)
+            imframe = im.copy()
+            if i == 0:
+                palette = imframe.getpalette()
+            else:
+                imframe.putpalette(palette)
+            yield imframe
+            i += 1
+    except EOFError:
+        pass
+
+for i, frame in enumerate(iter_frames(im)):
+    frame.save('test%d.png' % i,**frame.info)
